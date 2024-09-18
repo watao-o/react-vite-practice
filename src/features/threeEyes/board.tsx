1b467ef2fcc1c873e37f2cf6d7ff0afe7c701992
watao-o <thbc.watao@gmail.com>
2024-09-19 08:28:06 +0900

    add: React公式チュートリアル三目ならべ追加課題2

--- src/features/threeEyes/board.tsx
+++ src/features/threeEyes/board.tsx
@@ -24,24 +24,19 @@ const Board: React.FC<BoardProps> = ({xIsNext, squares, onPlay}) => {
     onPlay(nextSquares)
   }
 
+  const rows = [];
+  for (let i = 0; i < 3; i++) {
+    let squareList = [];
+    for (let j = 0;j < 3; j++) {
+      const index = i * 3 + j;
+      squareList.push(<Square value={squares[index]} onSquareClick={() => handleClick(index)}/>);
+    }
+    rows.push(<div className="board-row">{squareList}</div>);
+  }
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-      </div>
+      {rows}
     </>
   );
 }
